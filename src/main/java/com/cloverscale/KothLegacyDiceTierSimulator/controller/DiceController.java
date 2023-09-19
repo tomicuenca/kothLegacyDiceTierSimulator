e9161622627c2c4c03228252d9129f1c1a010587
Tomás Cuenca <40648224+tomicuenca@users.noreply.github.com>
2023-09-19 17:49:31 -0300

    Controlador actualizado con nuevo endpoint

--- src/main/java/com/cloverscale/KothLegacyDiceTierSimulator/controller/DiceController.java
+++ src/main/java/com/cloverscale/KothLegacyDiceTierSimulator/controller/DiceController.java
@@ -35,8 +35,6 @@ public class DiceController {
         Integer critElemPosAvg = diceService.getCritElemPosAvg(diceList, rolls);
         Integer critElemNegAvg = diceService.getCritElemNegAvg(diceList, rolls);
 
-
-
         str.append("Dice: ");
         for (Dice dice: diceList){
             str.append(dice.getAmount() + "D" + dice.getFaces() + ", ");
@@ -82,6 +80,56 @@ public class DiceController {
         }
         str.append(elemNegAvg);
         str.append(System.lineSeparator());
+        str.append(System.lineSeparator());
+        if(rolls == 1){
+            str.append("Elemental positive with crit: ");
+        }else{
+            str.append("Elemental positive with crit avg: ");
+        }
+        str.append(critElemPosAvg);
+        str.append(System.lineSeparator());
+        if(rolls == 1){
+            str.append("Elemental negative with crit: ");
+        }else{
+            str.append("Elemental negative with crit avg: ");
+        }
+        str.append(critElemNegAvg);
+        return ResponseEntity.ok(str.toString());
+    }
+
+    @PostMapping(value = "/rollMainStats")
+    public ResponseEntity<String> rollMainStats(@RequestBody DiceJSON request) {
+        StringBuilder str = new StringBuilder();
+        Integer rolls = request.getRolls();
+        List<Dice> diceList = request.getDiceList();
+        Integer critAvg = diceService.getCritAvg(diceList, rolls);
+        Integer max = diceService.getMax(diceList);
+        Integer min = diceService.getMin(diceList);
+        Integer critElemPosAvg = diceService.getCritElemPosAvg(diceList, rolls);
+        Integer critElemNegAvg = diceService.getCritElemNegAvg(diceList, rolls);
+
+        str.append("Dice: ");
+        for (Dice dice: diceList){
+            str.append(dice.getAmount() + "D" + dice.getFaces() + ", ");
+        }
+        str.append(rolls);
+        if(rolls == 1){
+            str.append(" roll:");
+        }else{
+            str.append(" rolls:");
+        }
+        str.append(System.lineSeparator());
+        str.append(System.lineSeparator());
+        if(rolls == 1){
+            str.append("Crit: ");
+        }else{
+            str.append("Crit avg: ");
+        }
+        str.append(critAvg);
+        str.append(System.lineSeparator());
+        str.append("Max: " + max);
+        str.append(System.lineSeparator());
+        str.append("Min: " + min);
         str.append(System.lineSeparator());
         if(rolls == 1){
             str.append("Elemental positive with crit: ");
